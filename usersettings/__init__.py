fad1908f3bf744c66ab5cbd6d225d235b16d89bf
Jose A. Jimenez <jajimenezcarm@gmail.com>
2019-11-02 20:30:10 +0100

    Implemented application ID functions

--- usersettings/__init__.py
+++ usersettings/__init__.py
@@ -1,13 +1,55 @@
 # User Settings
 # Jose A. Jimenez (jajimenezcarm@gmail.com)
 
+import os
+import pathlib
+
+
+_app_id = None
+
+_user_home_dir_path = str(pathlib.Path.home())
+_app_settings_dir_path = None
+_app_settings_file_name = "settings.json"
+_app_settings_file_path = None
+
 
 def get_application_id() -> str:
-    pass
+    """Returns the working application ID.
+    """
+
+    global _app_id
+    return _app_id
 
 
 def set_application_id(id: str):
-    pass
+    """Sets the working application ID. An Exception is raised if the ID is not
+    valid.
+
+    Args:
+        id: New working application ID. The ID must be an alphanumeric word or
+            None.
+    """
+
+    global _app_id
+    global _user_home_dir_path, _app_settings_dir_path, _app_settings_file_path
+
+    if (id is not None and (type(id) != str or id == "" or " " in id or
+        "\n" in id or "\t" in id or "\r" in id)): \
+
+        raise Exception(
+            "Wrong ID. The ID must be an alphanumeric word or None."
+        )
+
+    _app_id = id
+
+    if id is None:
+        _app_settings_dir_path = None
+        _app_settings_file_path = None
+    else:
+        _app_settings_dir_path = os.path.join(_user_home_dir_path, "." + id)
+
+        _app_settings_file_path = os.path.join(_app_settings_dir_path,
+                                               _app_settings_file_name)
 
 
 def get_setting_value(id: str, default_value: str) -> str:
