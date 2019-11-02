e4742e1567f414ea78d4978d2b6f88728920804f
Jose A. Jimenez <jajimenezcarm@gmail.com>
2019-11-02 19:26:07 +0100

    Added initial structure

---
+++ usersettings/__init__.py
@@ -0,0 +1,26 @@
+# User Settings
+# Jose A. Jimenez (jajimenezcarm@gmail.com)
+
+
+def get_application_id() -> str:
+    pass
+
+
+def set_application_id(id: str):
+    pass
+
+
+def get_setting_value(id: str, default_value: str) -> str:
+    pass
+
+
+def set_setting_value(id: str, value: str):
+    pass
+
+
+def clear_setting(id: str):
+    pass
+
+
+def clear_all_settings():
+    pass
