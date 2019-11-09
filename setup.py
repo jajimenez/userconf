09d1db4cb5f848f1d65122b602bc7664cedcc566
Jose A. Jimenez <jajimenezcarm@gmail.com>
2019-11-10 00:44:21 +0100

    Added a Setup script for generating PIP packages

---
+++ setup.py
@@ -0,0 +1,30 @@
+# User Settings
+# Jose A. Jimenez (jajimenezcarm@gmail.com)
+
+# To generate the PIP packages, run this command:
+#     python3 setup.py sdist bdist_wheel
+
+import setuptools
+
+
+with open("README.md", "rt") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="usersettings",
+    version="0.1.0",
+    description="Python library to manage application user settings",
+    author="Jose A. Jimenez",
+    author_email="jajimenezcarm@gmail.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jajimenez/usersettings",
+    classifiers=[
+        "Programming Language :: Python 3",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X"
+    ],
+    python_requires=">=3.6",
+    packages=setuptools.find_packages()
+)
