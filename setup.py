5abb432c51b9ff69112adfa60f741f6b52ccc757
Jose A. Jimenez <jajimenezcarm@gmail.com>
2020-08-01 15:09:12 +0200

    Updated Setup file.

--- setup.py
+++ setup.py
@@ -4,24 +4,26 @@
 import setuptools
 
 
-with open("README.md", "rt") as f:
-    long_description = f.read()
+if __name__ == "__main__":
+    with open("README.md", "r") as f:
+        long_description = f.read()
 
-setuptools.setup(
-    name="userconf",
-    version="0.3.0",
-    description="Application user settings management library",
-    author="Jose A. Jimenez",
-    author_email="jajimenezcarm@gmail.com",
-    license="MIT",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/jajimenez/userconf",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-        "License :: OSI Approved :: MIT License"
-    ],
-    python_requires=">=3.6",
-    packages=setuptools.find_packages()
-)
+    setuptools.setup(
+        name="userconf",
+        version="0.3.0",
+        description="Application user settings management library",
+        author="Jose A. Jimenez",
+        author_email="jajimenezcarm@gmail.com",
+        license="MIT",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/jajimenez/userconf",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+            "License :: OSI Approved :: MIT License"
+        ],
+        python_requires=">=3.6",
+        packages=["userconf"],
+        package_dir={"userconf": "src/userconf"}
+    )
